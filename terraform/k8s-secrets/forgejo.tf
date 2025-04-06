cc01e23e1a7e2164bd2946a123b7c295e317d4c5
Pierre-Olivier Rey <po@prrlvr.fr>
2025-04-06 21:55:59 +0200

    k8s: cnpg: init cluster

---
+++ terraform/k8s-secrets/forgejo.tf
@@ -0,0 +1,13 @@
+resource "random_password" "forgejo-password" {
+  length = 64
+}
+
+resource "vault_generic_secret" "forgejo-admin-creds" {
+  path = "k8s-prrlvr-fr/forgejo/forgejo-admin-creds"
+  data_json = jsonencode({
+    username = "admin"
+    email    = "admin@prrlvr.fr"
+    password = random_password.forgejo-password.result
+  })
+}
+
--- terraform/k8s-secrets/postgres.tf
+++ terraform/k8s-secrets/postgres.tf
@@ -1,3 +1,26 @@
+locals {
+  users = [
+    "forgejo"
+  ]
+}
+
+resource "random_password" "postgres-password" {
+  for_each = toset(local.users)
+
+  length  = 32
+  special = false
+}
+
+resource "vault_generic_secret" "postgres-user" {
+  for_each = toset(local.users)
+
+  path = "k8s-prrlvr-fr/postgres/users/${each.value}"
+  data_json = jsonencode({
+    username = each.value
+    password = random_password.postgres-password[each.value].result
+  })
+}
+
 resource "random_password" "pgadmin-password" {
   length  = 32
   special = false
@@ -8,5 +31,18 @@ resource "vault_generic_secret" "pgadmin" {
   data_json = jsonencode({
     username = "admin@prrlvr.fr"
     password = random_password.pgadmin-password.result
+  })
+}
+
+resource "random_password" "postgres-superuser-password" {
+  length  = 32
+  special = false
+}
+
+resource "vault_generic_secret" "postgres-superuser" {
+  path = "k8s-prrlvr-fr/postgres/superuser"
+  data_json = jsonencode({
+    username = "superuser"
+    password = random_password.postgres-superuser-password.result
   })
 }
